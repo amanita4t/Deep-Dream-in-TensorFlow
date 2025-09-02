3ec07561789486a00512675f3f5581a38234ed50
amanita4t <amanueltekalign4@gmail.com>
2025-09-02 12:24:26 +0300

    tensorflow session and model created

--- main.py
+++ main.py
@@ -21,4 +21,20 @@ def main():
         #Extract 
         with zipfile.ZipFile(local_zip_file, 'r') as zip_ref:
             zip_ref.extractall(data_dir)
+    
+    model_fn = 'tensorflow_inception_graph.pb'
+    #Creating tensorflow session and creating the model
+    graph = tf.Graph()
+    sess = tf.IntereactivSession(graph=graph)
+    with tf.gfile.FastGFile(os.path.join(data_dir, model_fn), 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    t_input = tf.placeholder(np.float32, name='input') #define input
+    imagenet_mean = 117.0
+    t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
+    tf.import_graph_def(graph_def, {'imput': t_preprocessed})
+
+    layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and '/import' in op.name]
+    feature_nums = [int(graph.get_tensor_by_name(name+ ':0').get_shape()[-1]) for names in layers]
+    
             
